ada068d271c0296a353eac1ca33f82c1bcda6c80
vintg <10236284+vintg@users.noreply.github.com>
2025-07-06 00:40:07 -0400

    refactor(App): update with Sheet component demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Sheet } from './components/Sheet'
+import { useSheetStore } from './store/useSheetStore'
+import type { ApiResponse } from './types/api'
+import mockData from './api/mockData.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const loadApiData = useSheetStore((data) => data.initFromApi)
+
+  useEffect(() => {
+    loadApiData(mockData as ApiResponse)
+  }, [loadApiData])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-screen h-screen flex flex-col">
+        <Sheet />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
